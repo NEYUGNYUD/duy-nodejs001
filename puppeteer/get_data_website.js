98132b76623122b7a49acae444ad33299232fabb
Nguyen Van Duy <duypeo96@gmail.com>
2017-11-08 17:26:19 +0700

    fix error received response and divide a file for using puppeteer

--- puppeteer/get_data_website.js
+++ puppeteer/get_data_website.js
@@ -1,7 +1,46 @@
 /**
  * Created by duype on 11/5/2017.
  */
+const url = require('url');
+const puppeteer = require('puppeteer');
 
-exports.getDateTime = function() {
-    return Date();
-};
+var getDataWeb = async function(req) {
+        //store information of response
+        let resInfor = {};
+        let addr = await url.parse(req.url, true);
+        let browser = await puppeteer.launch({headless: false});
+        let page = await browser.newPage();
+        try {
+            let resTem = await page.goto("http://www." + addr.query.p);//return response
+            if (resTem.status == 200) {
+                resInfor.sucess = 'true';
+                resInfor.message = 'ok';
+                resInfor.data = {};
+                //get cookies
+                let temp = await page.cookies(page.url());
+                resInfor.data.cookie = temp;
+                //get header
+                temp = await resTem.headers;
+                resInfor.data.header = temp;
+                //get html
+                temp = await page.content();
+                resInfor.data.html = temp;
+                // console.log(temp);
+                console.log('response received');
+                browser.close();
+                return resInfor;
+            } else {
+                console.log('***Error: Response is not received succeccful***');
+                resInfor.sucess = 'false';
+                resInfor.message = 'Không thể khởi động selenium/phantomjs/puppeteer.';
+                browser.close();
+                return resInfor;
+            }
+        } catch (error) {
+            resInfor.sucess = 'false';
+            resInfor.message = 'Không thể khởi động selenium/phantomjs/puppeteer.';
+            browser.close();
+            return resInfor;
+        }
+    };
+module.exports.getData = getDataWeb;
--- server.js
+++ server.js
@@ -1,45 +1,13 @@
 var http = require('http');
-var url = require('url');
-const puppeteer = require('puppeteer');
+var getDataWeb = require('./puppeteer/get_data_website');
+// const puppeteer = require('puppeteer');
 
 http.createServer(async function (req, res) {
-    //store information of response
-    let resInfor = {};
-    let addr = url.parse(req.url, true);
-    // await puppeteer.launch({headless: false}).then(async browser => {
-    let browser = await puppeteer.launch({headless: false});
-    let page = await browser.newPage();
-    try {
-        let resTem = await page.goto("http://www." + addr.query.p);//return response
-        if (resTem.status == 200) {
-            resInfor.sucess = 'true';
-            resInfor.message = 'ok';
-            resInfor.data = {};
-            //get cookies
-            let temp = await page.cookies(page.url());
-            resInfor.data.cookie = temp;
-            //get header
-            temp = await resTem.headers;
-            resInfor.data.header = temp;
-            //get html
-            temp = await page.content();
-            resInfor.data.html = temp;
-            console.log(temp);
-            console.log('response received');
-        } else {
-            console.log('***Error: Response is not received succeccful***');
-            resInfor.sucess = 'false';
-            resInfor.message = 'Không thể khởi động selenium/phantomjs/puppeteer.';
-        }
-    } catch (error) {
-        console.log('***Have an exception***');
-        resInfor.sucess = 'false';
-        resInfor.message = 'Không thể khởi động selenium/phantomjs/puppeteer.';
-    }
-    await browser.close();
-    // });
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.write(JSON.stringify(resInfor));
+    var resInfor = await getDataWeb.getData(req);
+    var jsonResInfor = JSON.stringify(resInfor);
+    res.writeHead(200, {'Content-Type':'application/json'});
+    // res.write(resInfor);
+    res.write(jsonResInfor);
     res.end();
 }).listen(80);
 console.log('port 80');
