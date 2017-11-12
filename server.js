5516c31aadd3a991080041e5ad167c65a986f6d7
Nguyen Van Duy <duypeo96@gmail.com>
2017-11-12 20:35:46 +0700

    call module external

--- server.js
+++ server.js
@@ -1,17 +1,6 @@
-const http = require('http');
-const url = require('url');
-var getDataWeb = require('./puppeteer/get_data_website');
-// const puppeteer = require('puppeteer');
+const server = require('./puppeteer/create_server.js');
+const route = require('./puppeteer/route');
+const processScript = require('./puppeteer/process_script');
+const processData = require('./puppeteer/process_data');
 
-http.createServer(async function (req, res) {
-    let responseInfor;
-    let pars = await url.parse(req.url, true).query;//url consist of one parameter named 'type' represents type of returned data
-    await getDataWeb.getData(pars, (resInfor) => {
-        responseInfor = JSON.stringify(resInfor);
-        res.writeHead(200, {'Content-Type':'application/json'});
-        res.write(responseInfor);
-        res.end();
-    });
-}).listen(80);
-console.log('port 80');
-
+server.start(route.route, processData.returnedResult, processScript.processScript);
