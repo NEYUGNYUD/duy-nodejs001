54f43f41fbd6730ec5ba2b9922806b302e5d56a5
Nguyen Van Duy <duypeo96@gmail.com>
2017-11-12 20:36:08 +0700

    create a script file for testing

--- puppeteer/script.js
+++ puppeteer/script.js
@@ -1,23 +1,7 @@
-var spt = [
-    {
-        "type": "visit",
-        "url": "http://www.techrum.vn/login/"
-    },
-    {
-        "type": "input",
-        "selector": "#ctrl_pageLogin_password",
-        "value": "1234567"
-    },
-    {
-        "type": "input",
-        "selector": "#ctrl_pageLogin_login",
-        "value": "duypeo96@hotmail.com"
-    },
-    {
-        "type": "submit",
-        "selector": "#pageLogin > dl.submitUnit > dd > input.button.primary",
-        "action": "click"
-    }
-];
+var spt ='[{"type": "visit", "url": "https://kipalog.com/users/sign_in"}, {"type":"input", "selector":"#password", "value":"123456789"},{"type": "input", "selector":"#email", "value":"duypeo96@gmail.com"}, {"type": "submit", "selector": "#signin", "action": "click"}]';
 
-module.exports.sp = spt;
+var encodeScript = encodeURIComponent(spt);
+console.log(encodeScript);
+
+//encodeScripts
+%5B%7B"type"%3A%20"visit"%2C%20"url"%3A%20"https%3A%2F%2Fkipalog.com%2Fusers%2Fsign_in"%7D%2C%20%7B"type"%3A"input"%2C%20"selector"%3A"%23password"%2C%20"value"%3A"123456789"%7D%2C%7B"type"%3A%20"input"%2C%20"selector"%3A"%23email"%2C%20"value"%3A"duypeo96%40gmail.com"%7D%2C%20%7B"type"%3A%20"submit"%2C%20"selector"%3A%20"%23signin"%2C%20"action"%3A%20"click"%7D%5D
